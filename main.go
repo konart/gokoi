217ec09465ecd208750a07118dd871e031fac07b
Artem Kondratev <konapt@gmail.com>
2017-02-12 21:39:13 +0300

    Added prepare method for Deck

--- main.go
+++ main.go
@@ -8,27 +8,11 @@ func main() {
 
 	var game Game
 	var deck Deck
-
 	deck = make([]*Card, 48)
-	i := 0
-	for key, value := range cards {
-		for _, group := range value {
-			deck[i] = &Card{key, group}
-			i++
-		}
-	}
-	deck.Shuffle()
+	deck.Prepare()
 	for _, card := range deck {
 		fmt.Println(*card)
 	}
-
-	_ = *deck.PickCard()
-	_ = *deck.PickCard()
-	_ = *deck.PickCard()
-
-	fmt.Print(len(deck))
-
-
 
 	game.deck = deck
 }
